19f2d09c67ae3a34b9c38ca9b7aea5ea229fd8e0
Raphaël Charrat <mrraph_@MacBouc.local>
2018-08-22 17:18:07 +0200

    Fixed silly typo ...

--- traefik-proxy/run.sh
+++ traefik-proxy/run.sh
@@ -14,8 +14,8 @@ ACME_EMAIL=$(jq --raw-output ".ACME_EMAIL // empty" $CONFIG_PATH)
 
 mkdir -p ${ACME_PATH}
 
-sed -i "s/##BASE_DOMAIN##/${BASE_DOMAIN}" /traefik.toml
-sed -i "s/##ACME_EMAIL##/${ACME_EMAIL}" /traefik.toml
+sed -i "s/##BASE_DOMAIN##/${BASE_DOMAIN}/" /traefik.toml
+sed -i "s/##ACME_EMAIL##/${ACME_EMAIL}/" /traefik.toml
 
 cp /traefik.toml ${TRAEFIK_CONFIG_PATH}
 
