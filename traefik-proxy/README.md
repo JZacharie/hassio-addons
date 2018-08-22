17dd18742f9ed7b75aaf1eb514feb9338f646575
Raphaël Charrat <mrraph_@MacBouc.local>
2018-08-22 17:02:35 +0200

    Added Traefik configuration

--- traefik-proxy/README.md
+++ traefik-proxy/README.md
@@ -1,1 +1,14 @@
-#hassio-hubot
+#traefik-proxy
+
+
+## Prerequisites
+
+This addon requires the addon [Docker Enabler](https://github.com/hassio-addons/addon-docker-enabler/blob/master/README.md) to be installed first.
+
+You should configure it to give Docker's API access to Traefik Proxy.
+
+````
+{
+  "target": "local_hassio_traefik"
+}
+````
--- traefik-proxy/run.sh
+++ traefik-proxy/run.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 #set -e
 CONFIG_PATH=/data/options.json
+TRAEFIK_CONFIG_PATH=/data/traefik.toml
 ACME_PATH=/data/acme
 
-#rm -rf *
-#mkdir -p bot && chmod -R 777 bot && cd bot
 DEFAULT_BASE_DOMAIN=""
 DEFAULT_ACME_EMAIL=""
-DEFAULT_ACME_DOMAINS=""
+#DEFAULT_ACME_DOMAINS="''"
 
-BASE_DOMAIN=$(jq --raw-output ".DEFAULT_BASE_DOMAIN // empty" $CONFIG_PATH)
-ACME_EMAIL=$(jq --raw-output ".DEFAULT_ACME_EMAIL // empty" $CONFIG_PATH)
-ACME_DOMAINS=$(jq --raw-output ".DEFAULT_ACME_DOMAINS // empty" $CONFIG_PATH)
+BASE_DOMAIN=$(jq --raw-output ".BASE_DOMAIN // empty" $CONFIG_PATH)
+ACME_EMAIL=$(jq --raw-output ".ACME_EMAIL // empty" $CONFIG_PATH)
+#ACME_DOMAINS=$(jq --raw-output ".ACME_DOMAINS // empty" $CONFIG_PATH)
 
-/traefik  --web --logLevel=DEBUG --docker --docker.domain=${BASE_DOMAIN} --docker.watch \
---acme --acme.storage=/etc/traefik/acme/acme.json acme.email=${ACME_EMAIL} --acme.entryPoint=https \
+mkdir -p ${ACME_PATH}
+
+sed -i "s/##BASE_DOMAIN##/${BASE_DOMAIN}" /traefik.toml
+sed -i "s/##ACME_EMAIL##/${ACME_EMAIL}" /traefik.toml
+
+cp /traefik.toml ${TRAEFIK_CONFIG_PATH}
+
+/traefik --web --logLevel=DEBUG --docker --docker.domain="${BASE_DOMAIN}" --docker.watch \
+--acme --acme.storage=${ACME_PATH}/acme.json acme.email="${ACME_EMAIL}" --acme.entryPoint=https \
 --entryPoints='Name:https Address::443 TLS' --entryPoints='Name:http Address::80 Redirect.EntryPoint:https' \
---defaultentrypoints=http,https --acme.domains=${ACME_DOMAINS}
+--defaultentrypoints=http,https --acme.domains="${ACME_DOMAINS}"
