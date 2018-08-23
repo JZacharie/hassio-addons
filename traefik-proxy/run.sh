b6e8d6eca28ea6157bfe906d23427d54fcfebd1d
Raphaël Charrat <mrraph_@MacBouc.lan>
2018-08-23 18:56:18 +0200

    Let's try this

--- traefik-proxy/run.sh
+++ traefik-proxy/run.sh
@@ -7,15 +7,21 @@ ACME_PATH=/data/acme
 DEFAULT_BASE_DOMAIN=""
 DEFAULT_ACME_EMAIL=""
 DEFAULT_ACME_ENABLED=""
+DEFAULT_SSL_CRT=""
+DEFAULT_SSL_KEY=""
 
 BASE_DOMAIN=$(jq --raw-output ".BASE_DOMAIN // empty" $CONFIG_PATH)
 ACME_EMAIL=$(jq --raw-output ".ACME_EMAIL // empty" $CONFIG_PATH)
 ACME_ENABLED=$(jq --raw-output ".ACME_ENABLED // empty" $CONFIG_PATH)
+SSL_CRT=$(jq --raw-output ".SSL_CRT // empty" $CONFIG_PATH)
+SSL_KEY=$(jq --raw-output ".SSL_KEY // empty" $CONFIG_PATH)
 
 mkdir -p ${ACME_PATH}
 
 sed -i "s/##BASE_DOMAIN##/${BASE_DOMAIN}/" /traefik.toml
 sed -i "s/##ACME_EMAIL##/${ACME_EMAIL}/" /traefik.toml
+sed -i "s/####SSL_CRT##/${SSL_CRT}/" /traefik.toml
+sed -i "s/##SSL_KEY##/${SSL_KEY}/" /traefik.toml
 
 if [[ ${ACME_ENABLED} -eq "true" ]]
 then
