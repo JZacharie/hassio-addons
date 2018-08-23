c24ceeccfb33ae96f0d31fc6bbc0280f97def5a4
Raphaël Charrat <mrraph_@in-twm-006.twm.ch>
2018-08-23 13:38:15 +0200

    Added an option to enable Let's Encrypt.

--- traefik-proxy/run.sh
+++ traefik-proxy/run.sh
@@ -6,16 +6,21 @@ ACME_PATH=/data/acme
 
 DEFAULT_BASE_DOMAIN=""
 DEFAULT_ACME_EMAIL=""
-#DEFAULT_ACME_DOMAINS="''"
+DEFAULT_ACME_ENABLED=""
 
 BASE_DOMAIN=$(jq --raw-output ".BASE_DOMAIN // empty" $CONFIG_PATH)
 ACME_EMAIL=$(jq --raw-output ".ACME_EMAIL // empty" $CONFIG_PATH)
-#ACME_DOMAINS=$(jq --raw-output ".ACME_DOMAINS // empty" $CONFIG_PATH)
+ACME_ENABLED=$(jq --raw-output ".ACME_ENABLED // empty" $CONFIG_PATH)
 
 mkdir -p ${ACME_PATH}
 
 sed -i "s/##BASE_DOMAIN##/${BASE_DOMAIN}/" /traefik.toml
 sed -i "s/##ACME_EMAIL##/${ACME_EMAIL}/" /traefik.toml
+
+if [[ ${ACME_ENABLED} -eq "true" ]]
+then
+    sed -i 's/^##ACME_ENABLED//' /traefik.toml
+fi
 
 cp /traefik.toml ${TRAEFIK_CONFIG_PATH}
 
